ee5d1861d7d85bb3d195e0c2ee801b30e2a6430b
vr-borra <vrblearn@gmail.com>
2024-10-28 17:53:11 -0500

    Refactor

--- savings_account.py
+++ savings_account.py
@@ -3,6 +3,7 @@
 from Account import Account
 from utils import calculate_interest
 
+
 # Define a function for the Savings Account
 def create_savings_account(balance, interest_rate, months):
     """Creates a savings account, calculates interest earned, and updates the account balance.
@@ -18,7 +19,7 @@ def create_savings_account(balance, interest_rate, months):
     """
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
     #  Hint: You need to add the interest as a value, i.e, 0.
-    account = Account(balance, interest_rate)
+    savings_account = Account(balance, interest_rate)
 
     # Calculate interest earned
     # interest_earned = float(balance * (interest_rate / 100 * months / 12))
@@ -28,10 +29,10 @@ def create_savings_account(balance, interest_rate, months):
     balance += interest_earned
 
     # Pass the updated_balance to the set balance method using the instance of the SavingsAccount class.
-    account.set_balance(balance)
+    savings_account.set_balance(balance)
 
     # Pass the interest_earned to the set interest method using the instance of the SavingsAccount class.
-    account.set_interest(interest_earned)
+    savings_account.set_interest(interest_earned)
 
     # Return the updated balance and interest earned.
-    return [float(account.balance), float(account.interest)]
+    return [float(savings_account.balance), float(savings_account.interest)]
